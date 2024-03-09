41d842e5fa10adefed2670164df7d0b37288f002
Ghanshyam Thakkar <shyamthakkar001@gmail.com>
2024-03-10 05:05:10 +0530

    export stats
    Signed-off-by: Ghanshyam Thakkar <shyamthakkar001@gmail.com>

--- lib/stats.go
+++ lib/stats.go
@@ -12,14 +12,19 @@ func Start() int64 {
 	return time.Now().UnixMilli()
 }
 
-// FinalStat prints the final stats of the transaction
-func FinalStat(TotalAmount uint64, startTime int64) {
+func GetStats(TotalAmount uint64, startTime int64) (float64, decor.SizeB1024, float64) {
 	currentTime := time.Now().UnixMilli()
-	timeDiff := float64(currentTime-startTime) / 1000.0
+	timeTaken := float64(currentTime-startTime) / 1000.0
 	totalAmountInMiB := float64(TotalAmount) / 1048576.0
+	speed := totalAmountInMiB / timeTaken
+	return timeTaken, decor.SizeB1024(TotalAmount), speed
+}
 
+// FinalStat prints the final stats of the transaction
+func FinalStat(totalAmount uint64, startTime int64) {
+	timeTaken, amount, speed := GetStats(totalAmount, startTime)
 	fmt.Printf("\nStats:\n")
-	fmt.Printf("Time Taken: %.2f seconds\n", timeDiff)
-	fmt.Printf("Total Amount Transferred: % .2f \n", decor.SizeB1024(TotalAmount))
-	fmt.Printf("Average Speed: %.2f MiB/s\n", totalAmountInMiB/timeDiff)
+	fmt.Printf("Time Taken: %.2f seconds\n", timeTaken)
+	fmt.Printf("Total Amount Transferred: % .2f \n", amount)
+	fmt.Printf("Average Speed: %.2f MiB/s\n", speed)
 }
